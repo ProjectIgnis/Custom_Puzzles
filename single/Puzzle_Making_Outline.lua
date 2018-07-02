4841806f2698169d4b1e28dc6c263e7f36709d21
AlphaKretin <deewhydeeecks@gmail.com>
2018-07-02 14:27:15 +0930

    Update Puzzle_Making_Outline.lua
    Proofreading and more explanations

--- single/Puzzle_Making_Outline.lua
+++ single/Puzzle_Making_Outline.lua
@@ -1,22 +1,23 @@
-Debug.SetAIName("AI") --Set's the AI name
-Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI,4) --Begin loading the field for a puzzle, with DUEL_ constants in (int flag) under Master Rule (int rule) Most use for puzzles is attack first turn and simple AI, aka, vs a puzzle AI and allowing to attack.
---List of constrants:
---DUEL_TEST_MODE
---DUEL_ATTACK_FIRST_TURN
---DUEL_NO_CHAIN_HINT
---DUEL_OBSOLETE_RULING
---DUEL_PSEUDO_SHUFFLE
---DUEL_TAG_MODE
---DUEL_SIMPLE_AI
---SPEED_DUEL
+Debug.SetAIName("AI") --Sets the AI name
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI,4) --Begin loading the field for a puzzle, with DUEL_ constants in (int flag) under Master Rule (int rule). 
+--The most used constants for puzzles are attack first turn (so you can attack) and simple AI (so the puzzle has some AI)
+--List of constants:
+--DUEL_TEST_MODE - The player controls the AI. Use for testing, not for challenge puzzles.
+--DUEL_ATTACK_FIRST_TURN - Allows the player to attack. Essential for one-turn challenge puzzles.
+--DUEL_NO_CHAIN_HINT - Irrelevant for challenge puzzles.
+--DUEL_OBSOLETE_RULING - First turn draw and "ignition priority". Irrelevant for challenge puzzles.
+--DUEL_PSEUDO_SHUFFLE - Don't shuffle deck. Depending on your puzzle, this may be necessary, or you may not want it.
+--DUEL_TAG_MODE - Tag duel. Irrelevant for one-turn challenge puzzles.
+--DUEL_SIMPLE_AI - AI player will always activate or summon cards whenever possible, without thinking about strategy. Can be useful in puzzles with care.
+--SPEED_DUEL - Speed duel format, 3 zones instead of 5.
 
-Debug.SetPlayerInfo(0,8000,0,0) -- Player's Lifepoints, Set opening hand, and draw phase
-Debug.SetPlayerInfo(1,8000,0,0) -- AI's Lifepoints, Set opening hand, and draw phase
---Contents here like add card
-Debug.ReloadFieldEnd() --Reload's the field to add everything. ALWAYS NEEDED with reloadfieldbegin
-aux.BeginPuzzle() --Start's it as a puzzle. Remove to allow muti turns / debugging.
+Debug.SetPlayerInfo(0,8000,0,0) --Player's Life Points, size of opening hand, and amount of cards in draw phase
+Debug.SetPlayerInfo(1,8000,0,0) --AI's Life Points, size of opening hand, and amount of cards in draw phase
 
+--Create the game state here, with functions like Debug.AddCard()
 
+Debug.ReloadFieldEnd() --Reloads the field to save the changes you specified above.
+aux.BeginPuzzle() --Set up game loss at end of turn for a one-turn challenge puzzle. Remove for multiple turns or testing.
 
 --LOCATION_DECK  
 --LOCATION_SZONE  
